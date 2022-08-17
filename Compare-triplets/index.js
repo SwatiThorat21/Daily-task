a8df93e3c65166df08e871a52b447d54a9f7a517
Swati <swatic019@gmail.com>
2022-08-17 23:04:07 +0530

    created compareTriplets function

--- Compare-triplets/index.js
+++ Compare-triplets/index.js
@@ -1,23 +1,20 @@
+function compareTriplets(a, b) {
+  let result = [];
+  let aScore = 0;
+  let bScore = 0;
 
-function compareTriplets(a,b){
-    
-let Alice = [a,a,a];
-let Bob = [b,b,b];
-let i;
-if( i >= 1 & i <= 100){
-if(a[i]>b[i]){
-    return Alice+1;
-}
-else if(a[i]<b[i]){
-    return Bob+1;
-}
-else if(a[i]==b[i]){
-    return 0;
-}
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) {
+      aScore++;
+    }
+    if (b[i] > a[i]) {
+      bScore++;
+    }
+  }
+
+  result[0] = aScore;
+  result[1] = bScore;
+  return result;
 }
 
-}
-
-console.log(compareTriplets([1,2,3],[3,2,1]))
-
-
+console.log(compareTriplets([1, 2, 3], [3, 2, 1]));
